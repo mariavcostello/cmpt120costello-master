cd1fa7c5e771d977ab00caf28632eb71fa20288d
maria <maria.costello1@marist.edu>
2018-02-23 15:52:55 -0500

    Added the 4th lab assignment.

---
+++ credentials.py
@@ -0,0 +1,27 @@
+# CMPT 120 Intro to Programming
+# Lab #5 – Working with Strings and Functions
+# Author: Maria C
+# Created: 2018-02-21
+def main():
+# get user's first and last names
+    first = input("Enter your first name, all lowercase: ")
+    last = input("Enter your last name, all lowercase: ")
+# TODO modify this to generate a Marist-style username
+    uname = first[:10] + ('.') + last[:9] + ('1')
+    print("Username is: ", uname)
+# ask user to create a new password
+    passwd = input("Create a new password: ")
+# TODO modify this to ensure the password has at least 8 characters
+def passwd ():
+    
+   while False:
+
+    print("Fool of a Took! That password is feeble!")
+    if passwd(len) <8:
+        passwd == input("Create a new password: ")
+
+    print("The force is strong in this one…")
+    if passwd(len) >8:
+        print("Account configured. Your new email address is", uname + "@marist.edu")
+    
+main()
