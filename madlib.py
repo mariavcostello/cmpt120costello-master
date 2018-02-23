d76afd644a8e3d8d3b6159afd9509f5bcc74531b
maria <maria.costello1@marist.edu>
2018-02-23 13:08:18 -0500

    Work in progress

---
+++ madlib.py
@@ -0,0 +1,9 @@
+#Mad Libs program
+def main():
+    noun = input("Enter a noun: ")
+    verb = input("Enter a verb: ")
+    adjective = input("Enter an adjective: ")
+    place = input("Enter a place: ")
+    print("Take your",adjective,noun," and ",verb," to the ",place,"!")
+
+main()
