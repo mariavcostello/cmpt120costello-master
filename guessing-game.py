6a4add8829958df5316ee36808bb6d27a1653b5d
mariavcostello <35709434+mariavcostello@users.noreply.github.com>
2018-03-05 15:43:51 -0500

    Add files via upload
    Guessing game

---
+++ guessing-game.py
@@ -0,0 +1,32 @@
+# CMPT 120 Intro to Programming
+# Lab #5 – Working with Strings and Functions
+# Author: Maria C
+# Created: 2018-03-2
+
+import random
+
+Animal = ('frog')
+correct = Animal
+count = 0
+
+def main ():
+   print ('I am thinking of an animal!')
+   Animal = input("Enter first guess:")
+def guess ():
+   while False:
+       
+    print ("No, that is not correct. Hint, the animal is green.")
+    if guess 
+        guess == input("Enter first guess:")
+    if guess == "frog" :
+        print ('Yes! That is correct!!!')
+
+    
+def counter ():
+    counter=0
+    while True:
+        Animal= str(Animal, count)   
+
+    
+main ()
+    
