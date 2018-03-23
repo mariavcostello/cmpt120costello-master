04630bfea7f5832ccd920bd8a5fd80f0b991b67a
maria <maria.costello1@marist.edu>
2018-03-23 12:14:05 -0400

    Added the new assignment (6)

---
+++ ArithmiticEngine2.py
@@ -0,0 +1,35 @@
+
+# CMPT 120 - Lab #6
+# YOUR NAME
+# DD-MMM-YYYY
+###
+
+def showIntro():
+       print("Welcome to the Arithmetic Engine!")
+       print("=================================\n")
+       print("Valid commands are 'add', 'mult', 'sub', 'div', and 'quit'.\n")
+def showOutro():
+       print("\nThank you for using the Arithmetic Engine…")
+       print("\nPlease come back again soon!")
+def doLoop():
+       while True:
+            cmd = input("What computation do you want to perform? ")
+            num1 = int(input("Enter the first number: "))
+            num2 = int(input("Enter the second number: "))
+            if cmd == "add":
+                   result = num1 + num2
+            elif cmd == "sub":
+                   result = num1 - num2
+            elif cmd == "mult":
+                   result = num1 * num2
+            elif cmd == "div":
+                   result = num1 // num2
+            elif cmd == 
+            elif cmd == "quit":
+                   break
+            print("The result is " + str(result) + ".\n")
+def main():
+       showIntro()
+       doLoop()
+       showOutro()
+main()
