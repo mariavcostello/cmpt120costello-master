ee5d4093515a4827e5775a4eed007e47eba4c599
maria <maria.costello1@marist.edu>
2018-03-23 12:45:13 -0400

    Fixed mixed-case and invalid issue

--- ArithmiticEngine2.py
+++ ArithmiticEngine2.py
@@ -7,27 +7,31 @@
 def showIntro():
        print("Welcome to the Arithmetic Engine!")
        print("=================================\n")
-       print("Valid commands are 'add', 'mult', 'sub', 'div', and 'quit'.\n")
+       print("Valid commands are 'add', 'mult', 'sub', 'div', and 'quit'.\n")
+
 def showOutro():
        print("\nThank you for using the Arithmetic Engine…")
        print("\nPlease come back again soon!")
+       # fixed so that it will accept lowercase 
 def doLoop():
-       while True:
-            cmd = input("What computation do you want to perform? ")
-            num1 = int(input("Enter the first number: "))
-            num2 = int(input("Enter the second number: "))
-            if cmd == "add":
-                   result = num1 + num2
-            elif cmd == "sub":
-                   result = num1 - num2
-            elif cmd == "mult":
-                   result = num1 * num2
-            elif cmd == "div":
-                   result = num1 // num2
-            elif cmd == 
-            elif cmd == "quit":
-                   break
-            print("The result is " + str(result) + ".\n")
+        while True:
+        cmd = input("What computation do you want to perform? ")
+        cmdlower = cmd.lower()
+        if cmdlower == "add":
+            result = num1 + num2
+        elif cmdlower == "sub":
+            result = num1 - num2
+        elif cmdlower == "mult":
+            result = num1 * num2
+        elif cmdlower == "div":
+            try:
+                result = num1 // num2
+        elif cmdlower == "quit":
+            break
+        else:
+            print(cmdlower, "is an invalid command.")
+            continue
+        print("The result is " + str(result) + ".\n")
 def main():
        showIntro()
        doLoop()
