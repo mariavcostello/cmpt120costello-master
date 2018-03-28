635dc23f665e0cb816b2567bfd90a44605728ffb
maria <maria.costello1@marist.edu>
2018-03-28 13:14:32 -0400

    Solution to Curiosity rover exercise.

---
+++ rover.py
@@ -0,0 +1,9 @@
+#Maria Costello
+#Assignment 2
+#rover.py
+
+def main() :
+    print ("time= distance/speed")
+    x = 34000000/186000
+    print(x , "seconds to recieve a picture from Mars")
+main()
